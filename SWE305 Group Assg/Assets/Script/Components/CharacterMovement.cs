c18de1718add4728d9190660b8756db18b2f1c9b
Ong Cong Kin <congkinong0112@gmail.com>
2022-11-22 18:44:36 +0800

    script reallocated

--- SWE305 Group Assg/Assets/Script/Components/CharacterMovement.cs
+++ SWE305 Group Assg/Assets/Script/Components/CharacterMovement.cs
@@ -2,28 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterMovement : MonoBehaviour
+public class CharacterMovement : CharacterComponents
 {
-    public float moveSpeed = 5f;
 
+    public float moveSpeed = 5f;
     public Rigidbody2D rb;
-    public Animator animator;
+    //public Animator animator;
 
     Vector2 movement;
-
-    // Update is called once per frame
-    void Update()
+    
+    void Update() 
     {
         movement.x = Input.GetAxisRaw("Horizontal");
         movement.y = Input.GetAxisRaw("Vertical");
-        
+
         animator.SetFloat("Horizontal", movement.x);
         animator.SetFloat("Vertical", movement.y);
         animator.SetFloat("Speed", movement.sqrMagnitude);
     }
-    
+
     void FixedUpdate()
     {
         rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
     }
+
 }
