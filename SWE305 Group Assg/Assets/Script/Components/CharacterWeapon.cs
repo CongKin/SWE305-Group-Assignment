d843a64a7e73f734219afa4ef4a18459b4061152
Ong Cong Kin <congkinong0112@gmail.com>
2022-11-26 17:37:26 +0800

    Lvel 2  update
    wannaaaa diedieidue

--- SWE305 Group Assg/Assets/Script/Components/CharacterWeapon.cs
+++ SWE305 Group Assg/Assets/Script/Components/CharacterWeapon.cs
@@ -84,11 +84,6 @@ public class CharacterWeapon : CharacterComponents
 
     public void EquipWeapon(Weapon weapon)
     {
-        CurrentWeapon = Instantiate(weapon, weaponHolderPosition.position, weaponHolderPosition.rotation);
-        CurrentWeapon.transform.parent = weaponHolderPosition;
-        CurrentWeapon.SetOwner(character);     
-        WeaponAim = CurrentWeapon.GetComponent<WeaponAim>(); 
-
         if (CurrentWeapon != null)
         {
             WeaponAim.DestroyReticle(); // Each weapon has its own Reticle component
@@ -96,6 +91,10 @@ public class CharacterWeapon : CharacterComponents
             Destroy(CurrentWeapon.gameObject);
         }
 
+        CurrentWeapon = Instantiate(weapon, weaponHolderPosition.position, weaponHolderPosition.rotation);
+        CurrentWeapon.transform.parent = weaponHolderPosition;
+        CurrentWeapon.SetOwner(character);     
+        WeaponAim = CurrentWeapon.GetComponent<WeaponAim>(); 
 
         if (character.CharacterType == Character.CharacterTypes.Player)
         {
