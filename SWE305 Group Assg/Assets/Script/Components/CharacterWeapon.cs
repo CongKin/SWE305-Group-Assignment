41167e9fb1fc610610eb82add2d30f45cbbe4df7
Ong Cong Kin <congkinong0112@gmail.com>
2022-11-26 17:48:11 +0800

    UI update

--- SWE305 Group Assg/Assets/Script/Components/CharacterWeapon.cs
+++ SWE305 Group Assg/Assets/Script/Components/CharacterWeapon.cs
@@ -99,6 +99,7 @@ public class CharacterWeapon : CharacterComponents
         if (character.CharacterType == Character.CharacterTypes.Player)
         {
             UIManager.Instance.UpdateAmmo(CurrentWeapon.CurrentAmmo, CurrentWeapon.MagazineSize);
+            UIManager.Instance.UpdateWeaponSprite(CurrentWeapon.gameObject.transform.GetChild(0).GetComponent<SpriteRenderer>().sprite);
         }
     }
 }
