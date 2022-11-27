3e9e6d6d368313546c5b320379ca9f68fbfa2391
Lollipopz <christiantan1530@gmail.com>
2022-11-27 17:54:52 +0800

    lvl4 chnge

--- SWE305 Group Assg/Assets/Sprites_L4/ItemCollector.cs
+++ SWE305 Group Assg/Assets/Sprites_L4/ItemCollector.cs
@@ -7,6 +7,8 @@ public class ItemCollector : MonoBehaviour
 {
     public static int keys = 0;
 
+    [SerializeField] private AudioSource collectionSoundEffect;
+
     private void Start()
     {
         UIManager.Instance.UpdateKey(keys);
@@ -16,7 +18,7 @@ public class ItemCollector : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Key"))
         {
-            
+            collectionSoundEffect.Play();
             Destroy(collision.gameObject);
             keys++;
             UIManager.Instance.UpdateKey(keys);
